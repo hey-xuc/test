e77f7206a82cb597d6c27c5ca2bea4c8adb4269c
hey-xuc <13242087366@163.com>
2020-04-03 12:35:36 +0800

    Initial commit

---
+++ boards/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Topic
+
+class NewTopicForm(forms.ModelForm):
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'rows': 5, 'placeholder': '你想干啥？'}
+        ), 
+        max_length=4000,
+        help_text='最长不能超过4000字符'
+    )
+
+    class Meta:
+        model = Topic
+        fields = ['subject', 'message']
